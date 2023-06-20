f52411198f41b32e5013b878de1747034c71f63f
François THEVAND <57869857+fthvd@users.noreply.github.com>
2023-06-20 07:38:51 +0200

    Add files via upload

---
+++ i18n/qdrawEVT_fr.ts
@@ -0,0 +1,379 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>QdrawEVT</name>
+    <message>
+        <source>Point drawing tool</source>
+        <translation>Outil de dessin de points</translation>
+    </message>
+    <message>
+        <source>XY Point drawing tool</source>
+        <translation>Outil de dessin de points XY</translation>
+    </message>
+    <message>
+        <source>DMS Point drawing tool</source>
+        <translation>Outil de dessin de points en coordonnées DMS</translation>
+    </message>
+    <message>
+        <source>Input X:</source>
+        <translation>Entrez X:</translation>
+    </message>
+    <message>
+        <source>Input Y:</source>
+        <translation>Entrez Y:</translation>
+    </message>
+    <message>
+        <source>Line drawing tool</source>
+        <translation>Outil de dessin de lignes</translation>
+    </message>
+    <message>
+        <source>Rectangle drawing tool</source>
+        <translation>Outil de dessin de rectangles</translation>
+    </message>
+    <message>
+        <source>Circle drawing tool</source>
+        <translation>Outil de dessin de cercles</translation>
+    </message>
+    <message>
+        <source>Polygon drawing tool</source>
+        <translation>Outil de dessin de polygones</translation>
+    </message>
+    <message>
+        <source>Buffer drawing tool on the selected layer</source>
+        <translation>Outil de dessin de tampons sur la couche sélectionnée</translation>
+    </message>
+    <message>
+        <source>Polygon buffer drawing tool on the selected layer</source>
+        <translation>Outil de de dessin de tampon avec sélection polygone sur la couche sélectionnée</translation>
+    </message>
+    <message>
+        <source>Attributes copying tool</source>
+        <translation>Outil de copie d&apos;attributs</translation>
+    </message>
+    <message>
+        <source>No vector layer selected !</source>
+        <translation>Aucune couche vecteur sélectionnée !</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Réglages</translation>
+    </message>
+    <message>
+        <source>Perimeter</source>
+        <translation>Périmètre</translation>
+    </message>
+    <message>
+        <source>(works only with metric crs)</source>
+        <translation>(fonctionne uniquement avec un système métrique)</translation>
+    </message>
+    <message>
+        <source>Give a perimeter in m:</source>
+        <translation>Entrez un périmètre en m:</translation>
+    </message>
+    <message>
+        <source>Drawing</source>
+        <translation>Dessin</translation>
+    </message>
+    <message>
+        <source>Give a name to the feature:</source>
+        <translation>Nommez l&apos;entité:</translation>
+    </message>
+    <message>
+        <source>Drawings</source>
+        <translation>Dessins</translation>
+    </message>
+    <message>
+        <source>Left click to place a text.</source>
+        <translation>Clic gauche pour placer un texte.</translation>
+    </message>
+    <message>
+        <source>Left click to place a point.</source>
+        <translation>Clic gauche pour placer un point.</translation>
+    </message>
+    <message>
+        <source>Maintain the left click to draw a rectangle.</source>
+        <translation>Maintenir le clic gauche pour dessiner un rectangle.</translation>
+    </message>
+    <message>
+        <source>Maintain the left click to draw a circle. Simple Left click to give a perimeter.</source>
+        <translation>Maintenir le clic gauche pour dessiner un cercle. Clic gauche simple pour préciser un périmètre.</translation>
+    </message>
+    <message>
+        <source>Left click to place points. Right click to confirm.</source>
+        <translation>Clic gauche pour placer les points de la ligne. Clic droit pour valider.</translation>
+    </message>
+    <message>
+        <source>Select a vector layer in the Layer Tree, then select an entity on the map.</source>
+        <translation>Sélectionner une couche vecteur dans la liste des couches, puis sélectionner des entités sur la carte.</translation>
+    </message>
+    <message>
+        <source>Select a vector layer in the Layer Tree, then left click on attributes of this layer on the map. Right Click to confirm.</source>
+        <translation>Sélectionner une couche vecteur dans la liste des couches, puis clic gauche sur les attributs de la couche sur la carte. Clic droit pour valider.</translation>
+    </message>
+    <message>
+        <source>Possible only in a saved project. The layers will thus be created in an events sub-folder of the folder containing the project.</source>
+        <translation>La création n&apos;est possible que dans un projet enregistré. Les couches se créeront ainsi dans un sous-dossier &quot;evenements&quot; du dossier contenant le projet.</translation>
+    </message>
+    <message>
+        <source>Creation of event shapefile: </source>
+        <translation>Création des couches évènement : </translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation>Attention</translation>
+    </message>
+    <message>
+        <source>There is no active layer !</source>
+        <translation>Il n&apos;y a aucune couche active !</translation>
+    </message>
+    <message>
+        <source>You must give a non-null value for a point&apos;s or line&apos;s perimeter !</source>
+        <translation>Vous devez préciser un périmètre non-nul pour un point ou une ligne !</translation>
+    </message>
+    <message>
+        <source>There is no selected layer, or it is not vector nor visible !</source>
+        <translation>Aucune couche n&apos;est sélectionnée, ou celle-ci n&apos;est pas vectorielle ou n&apos;est pas visible !</translation>
+    </message>
+    <message>
+        <source>Radius</source>
+        <translation>Rayon</translation>
+    </message>
+    <message>
+        <source>Give a radius in m:</source>
+        <translation>Précisez un rayon en m:</translation>
+    </message>
+    <message>
+        <source>QdrawEVT - Settings</source>
+        <translation>QdrawEVT - Réglages</translation>
+    </message>
+    <message>
+        <source>Opacity</source>
+        <translation>Opacité</translation>
+    </message>
+    <message>
+        <source>Change the drawing color</source>
+        <translation>Changer la couleur du dessin</translation>
+    </message>
+    <message>
+        <source>Change text font</source>
+        <translation>Changer la police du texte</translation>
+    </message>
+    <message>
+        <source>Change text color</source>
+        <translation>Changer la couleur du texte</translation>
+    </message>
+    <message>
+        <source>Create the group and the event layers</source>
+        <translation>Créer le groupe et les couches évènement</translation>
+    </message>
+    <message>
+        <source>Error when creating shapefile:</source>
+        <translation>Erreur lors de la création de la couche :</translation>
+    </message>
+    <message>
+        <source>Convert event layers</source>
+        <translation>Convertir les couches évènement</translation>
+    </message>
+    <message>
+        <source>DMS Point Tool</source>
+        <translation>Outil de dessin de points en coordonnées DMS</translation>
+    </message>
+    <message>
+        <source>Latitude</source>
+        <translation>Latitude</translation>
+    </message>
+    <message>
+        <source>Longitude</source>
+        <translation>Longitude</translation>
+    </message>
+    <message>
+        <source>Direction</source>
+        <translation>Direction</translation>
+    </message>
+    <message>
+        <source>Degrees</source>
+        <translation>Degrés</translation>
+    </message>
+    <message>
+        <source>Minutes</source>
+        <translation>Minutes</translation>
+    </message>
+    <message>
+        <source>Seconds</source>
+        <translation>Secondes</translation>
+    </message>
+    <message>
+        <source>Invalid input !</source>
+        <translation>Entrée invalide !</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Erreur</translation>
+    </message>
+    <message>
+        <source>Decimal minutes</source>
+        <translation>Minutes décimales</translation>
+    </message>
+    <message>
+        <source>Area</source>
+        <translation>Surface</translation>
+    </message>
+    <message>
+        <source>Add to an existing layer</source>
+        <translation>Ajouter à une couche existante</translation>
+    </message>
+    <message>
+        <source>Give a size in m:</source>
+        <translation>Entrez une taille en m:</translation>
+    </message>
+    <message>
+        <source>Width:</source>
+        <translation>Largeur:</translation>
+    </message>
+    <message>
+        <source>Height:</source>
+        <translation>Hauteur:</translation>
+    </message>
+    <message>
+        <source>Rectangle size</source>
+        <translation>Taille du rectangle</translation>
+    </message>
+    <message>
+        <source>Creation of ods workbook completed:</source>
+        <translation>Création du classeur ods terminée&#xa0;:</translation>
+    </message>
+    <message>
+        <source>Open file?</source>
+        <translation>Ouvrir le fichier ?</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Continuer</translation>
+    </message>
+    <message>
+        <source>Drawing completed. Make a selection in its grip?</source>
+        <translation>Dessin terminé. Effectuer une sélection dans son emprise ?</translation>
+    </message>
+    <message>
+        <source>works only with metric crs</source>
+        <translation>fonctionne uniquement avec les crs métriques</translation>
+    </message>
+    <message>
+        <source>Selecting...</source>
+        <translation>Sélection en cours...</translation>
+    </message>
+    <message>
+        <source>You didn&apos;t click on a layer&apos;s attribute !</source>
+        <translation>Vous n&apos;avez pas cliqué sur l&apos;attribut d&apos;une couche !</translation>
+    </message>
+    <message>
+        <source>You must give a non-null value for a point&apos;s or line&apos;s perimeter!</source>
+        <translation>Vous devez donner une valeur non nulle pour le périmètre d&apos;un point ou d&apos;une ligne !</translation>
+    </message>
+    <message>
+        <source>There is no selected layer, or it is not vector nor visible!</source>
+        <translation>Il n&apos;y a pas de couche sélectionnée, ou elle n&apos;est ni vectorielle ni visible&#xa0;!</translation>
+    </message>
+    <message>
+        <source>Search results</source>
+        <translation>Résultats de la recherche</translation>
+    </message>
+    <message>
+        <source>Save this tab&apos;s results</source>
+        <translation>Sauvegarder l&apos;onglet sélectionné</translation>
+    </message>
+    <message>
+        <source>Save all results</source>
+        <translation>Sauvegarder tous les onglets</translation>
+    </message>
+    <message>
+        <source>Export the selection as a memory layer</source>
+        <translation>Exporter la sélection en tant que couche mémoire</translation>
+    </message>
+    <message>
+        <source>Zoom to selected attributes</source>
+        <translation>Zoomer sur les attributs sélectionnés</translation>
+    </message>
+    <message>
+        <source>Highlight feature&apos;s geometry</source>
+        <translation>Mettre en surbrillance la géométrie de l&apos;entité</translation>
+    </message>
+    <message>
+        <source>Highlight feature&apos;s centroid</source>
+        <translation>Mettre en surbrillance le centroïde de l&apos;entité</translation>
+    </message>
+    <message>
+        <source>Loading...</source>
+        <translation>Chargement...</translation>
+    </message>
+    <message>
+        <source>Rename window</source>
+        <translation>Renommer la fenêtre</translation>
+    </message>
+    <message>
+        <source>Enter a new title:</source>
+        <translation>Saisissez un nouveau titre&#xa0;:</translation>
+    </message>
+    <message>
+        <source>Layer name</source>
+        <translation>Nom de la couche</translation>
+    </message>
+    <message>
+        <source>Give a name to the layer:</source>
+        <translation>Donnez un nom à la couche&#xa0;:</translation>
+    </message>
+    <message>
+        <source>There is no selected feature!</source>
+        <translation>Il n&apos;y a pas d&apos;attribut sélectionné&#xa0;!</translation>
+    </message>
+    <message>
+        <source>Selected features</source>
+        <translation>Attributs sélectionnés</translation>
+    </message>
+    <message>
+        <source>Length</source>
+        <translation>Longueur</translation>
+    </message>
+    <message>
+        <source>Save in...</source>
+        <translation>Sauvegarder...</translation>
+    </message>
+    <message>
+        <source>OpenDocument Spreadsheet (*.ods)</source>
+        <translation>Feuille de calcul OpenDocument (*.ods)</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <source>The file can&apos;t be written.</source>
+        <translation>Le fichier ne peut pas être écrit.</translation>
+    </message>
+    <message>
+        <source>Maybe you don&apos;t have the rights or are trying to overwrite an opened file.</source>
+        <translation>Vous n&apos;avez peut-être pas les droits d&apos;écriture ou vous essayez d&apos;écraser un fichier ouvert.</translation>
+    </message>
+    <message>
+        <source>Saving?</source>
+        <translation>Sauvegarder ?</translation>
+    </message>
+    <message>
+        <source>Would you like to save results before exit?</source>
+        <translation>Souhaitez-vous enregistrer les résultats avant de quitter&#xa0;?</translation>
+    </message>
+    <message>
+        <source>Extracts in</source>
+        <translation>Extractions dans</translation>
+    </message>
+    <message>
+        <source>Extract</source>
+        <translation>Extrait</translation>
+    </message>
+    <message>
+        <source>Selection made in</source>
+        <translation>Sélection opérée dans</translation>
+    </message>
+</context>
+</TS>
